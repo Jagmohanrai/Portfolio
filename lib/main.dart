864f36eeb5fc52a785c5ed5699be5c44a2694636
Jagmohan Rai <aahhansingh@gmail.com>
2021-05-02 20:38:19 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PortfolioX',
+      title: 'Jagmohan Rai',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.orange,
